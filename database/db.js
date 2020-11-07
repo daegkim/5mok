d23422e89b456dde0f7c7d90a5210df71ad24bbe
sanggan <sttv4303@naver.com>
2020-11-07 17:31:38 +0900

    201107 push test

--- database/db.js
+++ database/db.js
@@ -10,7 +10,7 @@ class db {
 
     mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true}, (err) => { if(err) console.log(err) })
   }
-
+//
   getRoomList(callback){
     room.find({useFlag: true}).sort('roomId').exec()
     .then((res) => {
