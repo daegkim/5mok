141417eaa167e028aa78e864ee3625b092c074eb
daegeunkim <daegeunkim92@gmail.com>
2020-11-14 14:02:41 +0800

    2020.11.14 게임이 정상적으로 작동되도록 수정함

--- public/javascripts/socket_client.js
+++ public/javascripts/socket_client.js
@@ -93,150 +93,43 @@ socket.on('leave_room', (_room) => {
   }
 })
 
+socket.on('success_put_stone_on_map', (_data) => {
+  //1. 체크메이트인 경우
+  //1.1. 이긴 사람 화면 : 당신이 이겼습니다.
+  //1.2. 진 사람 화면 : 이긴사람ID가 이겼습니다. 
+  //1.3. 화면 초기화
+  //1.4. 게임 초기화
+  //2. 체크메이트 아닌 경우
+  //2.1. 말을 둔 사람과 상대편 모두 해당 위치에 말 표시
+  //3. undefined라면
+  //3.1. 둔 사람 화면만 : 해당 위치에 이미 있음
+  if(_data.result === undefined){
+    alert('해당 위치에 이미 말이 있습니다')
+    return
+  }
 
-//구버전
-/*
-socket.on('create_room', (data) => {
-    if(data.members[0] === socket.id){
-        room_id = data.room_id
-        document.getElementById('btnLeaveRoom').style.display = 'inline-block'
-        document.getElementById('divRoomList').style.display = 'none'
-        document.getElementById('div_board').style.display = 'block'
-        player = 1
-    }
-    
-    let btnNewRoom = createRoomButton(data.room_id, 1)
-    document.getElementById('divRoomList').appendChild(btnNewRoom)
+  turn = _data.turn
+  let color = _data.player === 1 ? 'black' : 'white'
+  $('#cell'+_data.pos).css('background-color', color)
+
+  //상대가 둔 돌은 깜빡거림
+  if(_data.player !== player){
+
+  }
+
+  if(_data.result){
+      init_board()
+      if(_data.player === player){
+          //socket.emit('end_game', room_id)
+          alert('당신이 이김')
+      }
+      else{
+          alert('당신이 짐')
+      }
+  }
 })
-
-socket.on('join_room', (data) => {
-    if(data.result){ //join success
-        document.getElementById(data.room_id).innerText = 'this room is full'
-
-        //same room
-        console.log(data, room_id)
-        if(data.room_id === room_id){
-            $('td').click(tb_click)
-            //this room member
-            if(data.socket_id === socket.id){
-                document.getElementById('div_board').style.display = 'block'
-                document.getElementById('divRoomList').style.display = 'none'
-                document.getElementById('btnLeaveRoom').style.display = 'block'
-                player = 2
-                alert(msg_welcome + data.room_id)
-            }     
-            //another room member
-            else{
-                player = 1
-                alert(socket.id + ' comes')
-            }
-        }
-    }
-    else{ //join fail
-        if(data.socket_id === socket.id){
-            room_id = -1
-            alert(msg_full)
-        }
-    }
-})
-
-socket.on('leave_room', (data) => {
-    if(data.isRoomDeleted){
-        deleteRoomButton(data.room_id)
-    }
-    else{
-        document.getElementById(data.room_id).innerHTML = '<p id=' + data.room_id + '>'+ 'member_num : 1' +  '</p>'
-    }
-
-    if(room_id === data.room_id){
-        $('td').off('click', tb_click)
-        init_board()
-        if(data.socket_id === socket.id){
-            room_id = -1
-            player = -1
-            document.getElementById('btnLeaveRoom').style.display = 'none'
-            document.getElementById('divRoomList').style.display = 'block'
-            document.getElementById('div_board').style.display = 'none'
-        }
-        else{
-            if(!data.isRoomDeleted){
-                socket.emit('end_game', data.room_id)
-            }
-            alert(data.socket_id + msg_leave)
-        }
-    }
-})
-
-socket.on('init_room_info', (data) => {
-    for(let i in data){
-        let btnNewRoom = createRoomButton(data[i].room_id, data[i].members.length)
-        document.getElementById('divRoomList').appendChild(btnNewRoom)
-    }
-})
-*/
-socket.on('putStoneOnMap', (data) => {
-    //1. 체크메이트인 경우
-    //1.1. 이긴 사람 화면 : 당신이 이겼습니다.
-    //1.2. 진 사람 화면 : 이긴사람ID가 이겼습니다. 
-    //1.3. 화면 초기화
-    //1.4. 게임 초기화
-    //2. 체크메이트 아닌 경우
-    //2.1. 말을 둔 사람과 상대편 모두 해당 위치에 말 표시
-    //3. undefined라면
-    //3.1. 둔 사람 화면만 : 해당 위치에 이미 있음
-    if(data.result === undefined && data.socket_id === socket.id){
-        alert('해당 위치에 이미 말이 있습니다')
-        return
-    }
-
-    turn = data.turn
-    let color = data.player === 1 ? 'black' : 'white'
-    $('#td'+data.pos).css('background-color', color)
-
-    //상대가 둔 돌은 깜빡거림
-    if(data.player !== player){
-        oppentStone = '#td'+data.pos
-        $('#td'+data.pos).addClass('stoneBlink')
-    }
-
-    if(data.result){
-        init_board()
-        if(data.player === player){
-            socket.emit('end_game', room_id)
-            alert('당신이 이김')
-        }
-        else{
-            alert('당신이 짐')
-        }
-    }
-})
-
-function tb_click(e){
-    //이미 바둑알이 있는 경우 둘 수 없음
-    if($('#'+e.target.id).css('background-color') !== 'rgba(0, 0, 0, 0)'){
-        alert('해당 위치에 이미 말이 있습니다.')
-        return
-    }
-
-    if(player !== turn){
-        alert('당신 차례가 아닙니다.')
-        return
-    }
-
-    x = e.target.parentElement.rowIndex
-    y = e.target.cellIndex
-
-    $(oppentStone).removeClass('stoneBlink')
-
-    socket.emit('putStoneOnMap', {
-        x: x,
-        y: y,
-        player: player,
-        socket_id: socket.id
-    })
-}
 
 function init_board(){
-    $('td').css('background-color', 'rgba(0, 0, 0, 0)')
+    $('#div-board-cell').css('background-color', 'rgba(0, 0, 0, 0)')
     turn = 1
 }
--- socket_app.js
+++ socket_app.js
@@ -31,6 +31,17 @@ var showRooms = (socket) => {
   io.to(socket.id).emit('send_rooms', roomsForClient) 
 }
 
+const getGameByRoomId = (_roomId) => {
+  var result = null
+  for(var room of rooms){
+    if(room.roomId === _roomId){
+      result = room.game
+      break
+    }
+  }
+  return result
+}
+
 io.on('connect', (socket) => {
   console.log('socket_app connected')
 
@@ -170,151 +181,21 @@ io.on('connect', (socket) => {
     }
   })
 
-  socket.on('put_stone_on_map', () => {
-    let room_id = getRoomIDBySocketID(socket.id)
-    let room_game = getGameByRoomId(room_id)
+  socket.on('put_stone_on_map', (_data) => {
+    let roomId = _data.roomId
+    let game = getGameByRoomId(roomId)
 
-    let result = room_game.putStoneOnMap(data.x, data.y, data.player)
-    let send_data = {
+    let result = game.putStoneOnMap(_data.x, _data.y, _data.player)
+    let sendData = {
       result: result,
-      pos: parseInt(data.x) * 18 + parseInt(data.y),
-      player: data.player,
+      pos: parseInt(_data.x) * 18 + parseInt(_data.y),
+      player: _data.player,
       socket_id: socket.id,
-      turn: data.player === 1 ? 2 : 1
+      turn: _data.player === 1 ? 2 : 1
     }
 
-    io.to('room' + room_id).emit('success_put_stone_on_map', send_data)
+    io.to('room' + roomId).emit('success_put_stone_on_map', sendData)
   })
-  /*
-    socket.on('join_room', (data) => {
-        let result = false
-
-        for(let i in room_info){
-            if(room_info[i].room_id === data){
-                if(room_info[i].members.length === 1){
-                    if(room_info[i].members[0] === socket.id){
-                        break
-                    }
-                    room_info[i].members.push(socket.id)
-                    socket.join('room' + data)
-                    result = true
-                }
-                break
-            }
-        }
-
-        io.emit('join_room',{
-            result: result,
-            socket_id: socket.id,
-            room_id: data
-        })
-    })
-
-    socket.on('disconnect', () => {
-        leave_room(socket)
-    })
-
-    socket.on('leave_room', () => {
-        let room_id = 'room' + getRoomIDBySocketID(socket.id)
-        socket.leave(room_id + room_id)
-        leave_room(socket)
-    })
-
-    socket.on('putStoneOnMap', (data) => {
-        let room_id = getRoomIDBySocketID(socket.id)
-        let room_game = getGameByRoomId(room_id)
-
-        let result = room_game.putStoneOnMap(data.x, data.y, data.player)
-        let send_data = {
-            result: result,
-            pos: parseInt(data.x) * 18 + parseInt(data.y),
-            player: data.player,
-            socket_id: socket.id,
-            turn: data.player === 1 ? 2 : 1
-        }
-
-        io.to('room' + room_id).emit('putStoneOnMap', send_data)
-    })
-
-    socket.on('end_game', (data) => {
-        getGameByRoomId(parseInt(data)).initMap()
-    })
-
-    io.to(socket.id).emit('init_room_info', room_info)
-    */
 })
 
-/*
-const leave_room = (socket) => {
-    let room_id = -1
-    let isRoomDeleted = false
-    let flag = false
-
-    for(var i in room_info){
-        for(var j in room_info[i].members){
-            if(room_info[i].members[j] === socket.id){
-                //delete the member from the room
-                room_id = room_info[i].room_id
-                room_info[i].members.splice(j, 1)
-                //delete the room
-                if(room_info[i].members.length === 0){
-                    room_info.splice(i, 1)
-                    isRoomDeleted = true
-                }
-                else{
-                    room_info[i].game.initMap()
-                }
-
-                flag = true
-                break
-            }
-        }
-        if(flag){
-            break
-        }
-    }
-
-
-
-    if(room_id > 0){
-        io.emit('leave_room', {
-            room_id: room_id,
-            socket_id: socket.id,
-            isRoomDeleted: isRoomDeleted
-        })
-    }
-}
-
-const getRoomIDBySocketID = (socket_id) => {
-    let result = ''
-    for(var i in room_info){
-        let flag = false
-        for(var j in room_info[i].members){
-            if(room_info[i].members[j] === socket_id){
-                result = room_info[i].room_id
-                break
-            }
-        }
-        if(flag){
-            break
-        }
-    }
-    return result
-}
-
-const getGameByRoomId = (_room_id) => {
-    let result = null
-    for(let i in room_info){
-        let flag = false
-        if(room_info[i].room_id === _room_id){
-            result = room_info[i].game
-            break
-        }
-        if(flag){
-            break
-        }
-    }
-    return result
-}
-*/
 module.exports = io;
